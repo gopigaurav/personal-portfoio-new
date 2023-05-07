123218594ec2936a449d351a40cc4d39d10020f9
gopi <gopigaurav9@gmail.com>
2023-05-07 22:04:08 +0530

    chanegex

--- pages/index.js
+++ pages/index.js
@@ -39,10 +39,10 @@ export default function Home() {
                 className="!text-6xl !text-left xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl"
               />
               <p className="my-4 text-base font-medium md:text-sm sm:text-xs ">
-                As a skilled full-stack developer, I am dedicated to turning
+                As a skilled Software developer, I am dedicated to turning
                 ideas into innovative web applications. Explore my latest
-                projects, showcasing my expertise in React.js and
-                web development.
+                projects, showcasing my expertise in web development and 
+                flexibility in learning any new technologies.
               </p>
               <div className="flex items-center self-start mt-2 lg:self-center">
                 <Link
