52f2f6b563ce8b32214ba2d7b59c7f0fa219db11
gopi <gopigaurav9@gmail.com>
2023-05-13 15:33:41 +0530

    added intro

--- Components/Transitions.js
+++ Components/Transitions.js
@@ -1,27 +1,35 @@
-import { motion } from 'framer-motion'
-import React from 'react'
-
+import { motion } from "framer-motion";
+import React from "react";
 
 const Transitions = () => {
   return (
     <>
-        <motion.div className='fixed top-0 right-0 bottom-full w-screen h-screen z-50 bg-primary'
-            initial={{y:"0%", height:"100%"}}
-            animate={{height:"0%"}}
-            transition={{duration:0.8,ease:"easeInOut"}}
-        />
-        {/* <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-40 bg-light'
-            initial={{x:"100%",width:"100%"}}
-            animate={{x:"0%",width:"0%"}}
-            transition={{delay:0.2,duration:0.8,ease:"easeInOut"}}
-        />
-        <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-slate-700'
-            initial={{x:"100%",width:"100%"}}
-            animate={{x:"0%",width:"0%"}}
-            transition={{delay:0.4,duration:0.8,ease:"easeInOut"}}
-        /> */}
+      <div>
+        <div className="ring w-36 h-36 text-lg text-black md:h-24 md:w-24 md:text-sm sm:h-20 sm:w-20 sm:text-xs">
+          WELCOME
+          <span></span>
+        </div>
+      </div>
+      <motion.div
+        className="fixed top-0 right-0 bottom-full w-screen h-screen z-50 bg-primary"
+        initial={{ y: "0%", height: "100%" }}
+        animate={{ height: "0%" }}
+        transition={{ duration: 0.8, ease: "easeInOut" }}
+      />
+      <motion.div
+        className="fixed top-0 right-0 bottom-full w-screen h-screen z-40 bg-light"
+        initial={{ y: "0%", height: "100%" }}
+        animate={{ height: "0%" }}
+        transition={{ delay: 0.2, duration: 0.8, ease: "easeInOut" }}
+      />
+      <motion.div
+        className="fixed top-0 right-0 bottom-full w-screen h-screen z-10 bg-slate-700"
+        initial={{ y: "0%", height: "100%" }}
+        animate={{ height: "0%" }}
+        transition={{ delay: 0.4, duration: 0.8, ease: "easeInOut" }}
+      />
     </>
-  )
-}
+  );
+};
 
-export default Transitions
+export default Transitions;
